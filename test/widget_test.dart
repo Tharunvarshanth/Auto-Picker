0f5d5d80e246c6b48d6ee749411d450573fcb97d
pssharvan1220 <sajeekiso1220@gmail.com>
2023-02-03 14:45:48 +0530

    Test cases for text

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,8 @@ import 'package:flutter_driver/flutter_driver.dart' as d;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:auto_picker/main.dart';
+import 'package:auto_picker/components/pages/profile_page.dart';
+import 'package:auto_picker/components/pages/product_page.dart';
 
 void main() {
   enableFlutterDriverExtension();
@@ -31,6 +33,131 @@ void main() {
       await tester.pumpWidget(const HomePage());
       final titleFinder = find.text('Mechanics');
       final messageFinder = find.text('M');
+
+      // Use the `findsOneWidget` matcher provided by flutter_test to verify
+      // that the Text widgets appear exactly once in the widget tree.
+      expect(titleFinder, findsOneWidget);
+      expect(messageFinder, findsOneWidget);
+    });
+
+    testWidgets('Home Page has a title and message', (tester) async {
+      await tester.pumpWidget(const HomePage());
+      final titleFinder = find.text('Products');
+      final messageFinder = find.text('P');
+
+      // Use the `findsOneWidget` matcher provided by flutter_test to verify
+      // that the Text widgets appear exactly once in the widget tree.
+      expect(titleFinder, findsOneWidget);
+      expect(messageFinder, findsOneWidget);
+    });
+  });
+
+  group('Profile Controller Test', () {
+    d.FlutterDriver driver;
+    setUpAll(() async {
+      // Connects to the app
+      driver = await d.FlutterDriver.connect();
+    });
+    tearDownAll(() async {
+      if (driver != null) {
+        // Closes the connection
+        driver.close();
+      }
+    });
+    testWidgets('Profile Controller has a title and data', (tester) async {
+      await tester.pumpWidget(const HomePage());
+      final titleFinder = find.text('Role');
+      final messageFinder = find.text('R');
+
+      // Use the `findsOneWidget` matcher provided by flutter_test to verify
+      // that the Text widgets appear exactly once in the widget tree.
+      expect(titleFinder, findsOneWidget);
+      expect(messageFinder, findsOneWidget);
+    });
+
+    testWidgets('Profile Controller has a title and data', (tester) async {
+      await tester.pumpWidget(const HomePage());
+      final titleFinder = find.text('Mobile Number');
+      final messageFinder = find.text('M');
+
+      // Use the `findsOneWidget` matcher provided by flutter_test to verify
+      // that the Text widgets appear exactly once in the widget tree.
+      expect(titleFinder, findsOneWidget);
+      expect(messageFinder, findsOneWidget);
+    });
+
+    testWidgets('Profile Controller has a title and data', (tester) async {
+      await tester.pumpWidget(const HomePage());
+      final titleFinder = find.text('City');
+      final messageFinder = find.text('C');
+
+      // Use the `findsOneWidget` matcher provided by flutter_test to verify
+      // that the Text widgets appear exactly once in the widget tree.
+      expect(titleFinder, findsOneWidget);
+      expect(messageFinder, findsOneWidget);
+    });
+
+    testWidgets('Profile Controller has a title and data', (tester) async {
+      await tester.pumpWidget(const HomePage());
+      final titleFinder = find.text('Address');
+      final messageFinder = find.text('A');
+
+      // Use the `findsOneWidget` matcher provided by flutter_test to verify
+      // that the Text widgets appear exactly once in the widget tree.
+      expect(titleFinder, findsOneWidget);
+      expect(messageFinder, findsOneWidget);
+    });
+  });
+
+  group('Product Page Test', () {
+    d.FlutterDriver driver;
+    setUpAll(() async {
+      // Connects to the app
+      driver = await d.FlutterDriver.connect();
+    });
+    tearDownAll(() async {
+      if (driver != null) {
+        // Closes the connection
+        driver.close();
+      }
+    });
+    testWidgets('Product Page has labels and data', (tester) async {
+      await tester.pumpWidget(const HomePage());
+      final titleFinder = find.text('Price');
+      final messageFinder = find.text('P');
+
+      // Use the `findsOneWidget` matcher provided by flutter_test to verify
+      // that the Text widgets appear exactly once in the widget tree.
+      expect(titleFinder, findsOneWidget);
+      expect(messageFinder, findsOneWidget);
+    });
+
+    testWidgets('Product Page has labels and data', (tester) async {
+      await tester.pumpWidget(const HomePage());
+      final titleFinder = find.text('Description');
+      final messageFinder = find.text('D');
+
+      // Use the `findsOneWidget` matcher provided by flutter_test to verify
+      // that the Text widgets appear exactly once in the widget tree.
+      expect(titleFinder, findsOneWidget);
+      expect(messageFinder, findsOneWidget);
+    });
+
+    testWidgets('Product Page has labels and data', (tester) async {
+      await tester.pumpWidget(const HomePage());
+      final titleFinder = find.text('Contact Detail');
+      final messageFinder = find.text('C');
+
+      // Use the `findsOneWidget` matcher provided by flutter_test to verify
+      // that the Text widgets appear exactly once in the widget tree.
+      expect(titleFinder, findsOneWidget);
+      expect(messageFinder, findsOneWidget);
+    });
+
+    testWidgets('Product Page has labels and data', (tester) async {
+      await tester.pumpWidget(const HomePage());
+      final titleFinder = find.text('Address');
+      final messageFinder = find.text('A');
 
       // Use the `findsOneWidget` matcher provided by flutter_test to verify
       // that the Text widgets appear exactly once in the widget tree.
